de1973a92318009bc651e21902d9cc06f9a073f2
Olav Markus Sjursø <olavms16@student.uia.no>
2018-05-15 20:58:40 +0200

    Migrated to BS4, added views from BS Studio

---
+++ hva_som_skjer/Controllers/EventController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using hva_som_skjer.Models;
+using hva_som_skjer.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace hva_som_skjer.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
