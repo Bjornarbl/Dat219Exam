f7e8f242352dc18cd20d7b7337307b192fb76af8
Olav Markus Sjursø <olavms16@student.uia.no>
2018-05-31 16:42:35 +0200

    Added api key

--- hva_som_skjer/Services/AuthMessageSenderOptions.cs
+++ hva_som_skjer/Services/AuthMessageSenderOptions.cs
@@ -1,5 +1,12 @@
 public class AuthMessageSenderOptions
 {
+        public AuthMessageSenderOptions ()
+        {
+                SendGridUser = "HvaSomSkjer";
+                SendGridKey = "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";
+        }
+
+
         public string SendGridUser { get; set; }
         public string SendGridKey { get; set; }
 }
